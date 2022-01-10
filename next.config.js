e9086664f95da5d1a8fcdaf874e01d6c8938de75
Rostyslav Sipakov <sipakov.rv@missioniz.com>
2022-01-10 02:48:59 -0500

    feat: install contentlayer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 const { i18n } = require('./next-i18next.config')
+const { withContentlayer } = require('next-contentlayer')
 
 module.exports = withPWA({
 	pwa: {
@@ -8,4 +9,5 @@ module.exports = withPWA({
 		runtimeCaching,
 	},
 	i18n,
+	withContentlayer
 })
