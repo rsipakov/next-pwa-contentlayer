cff43097f8b5624254c88d802e43cfcef107a7f7
Rostyslav Sipakov <sipakov.rv@missioniz.com>
2022-01-11 01:05:42 -0500

    fix: update webpack

--- next.config.js
+++ next.config.js
@@ -19,34 +19,6 @@ module.exports = withPlugins([
 				'pbs.twimg.com' // Twitter Profile Picture
 			]
 		},
-		webpack: (config, { dev, isServer }) => {
-			config.module.rules.push({
-				test: /\.(png|jpe?g|gif|mp4)$/i,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							publicPath: '/_next',
-							name: 'static/media/[name].[hash].[ext]',
-						},
-					},
-				],
-			})
-			config.module.rules.push({
-				test: /\.svg$/,
-				use: ['@svgr/webpack'],
-			})
-			if (!dev && !isServer) {
-				// Replace React with Preact only in client production build
-				Object.assign(config.resolve.alias, {
-					'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-					react: 'preact/compat',
-					'react-dom/test-utils': 'preact/test-utils',
-					'react-dom': 'preact/compat',
-				})
-			}
-			return config;
-		},
 		pwa: {
 			dest: 'public',
 			runtimeCaching
