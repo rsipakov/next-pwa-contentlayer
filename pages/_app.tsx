06b24fa68197321b3850e5588fb5a230b4c2f2bc
Rostyslav Sipakov <sipakov.rv@missioniz.com>
2022-01-12 01:07:47 -0500

    refactor: move meta to the _document.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
-import Meta from '@/components/meta'
 import '@/styles/globals.css'
 import { appWithTranslation } from 'next-i18next'
 
@@ -11,10 +10,11 @@ const App = ({ Component, pageProps }: AppProps) => {
 			defaultTheme='system'
 			disableTransitionOnChange
 		>
-			<Meta />
 			<Component {...pageProps} />
 		</ThemeProvider>
 	)
 }
 
 export default appWithTranslation(App);
+
+// Reference for "next-themes": https://www.npmjs.com/package/next-themes
