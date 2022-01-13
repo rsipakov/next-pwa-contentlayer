544561ffcec627fa93c63cdd5287803fb639c476
Rostyslav Sipakov <sipakov.rv@missioniz.com>
2022-01-13 02:25:10 -0500

    chore: add sire url

--- data/siteMetaData.js
+++ data/siteMetaData.js
@@ -4,7 +4,7 @@ const siteMetaData = {
 	storyPageName: { en: 'Story', ru: 'Истории' },
 	blogPageName: { en: 'Blog', ru: 'Блог'},
 	siteDescription: { en: 'This is a website about Red Cap', ru: 'Это вебсайт о Красной Шапке' },
-	siteUrl: 'next-pwa-contentlayer.vercel.app',
+	siteUrl: 'https://next-pwa-contentlayer.vercel.app',
 	socialBanner: '',
 	twitterBanner: ''
 }
