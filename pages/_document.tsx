943188c4ad271635dec372d6386da3b945cc63e9
Rostyslav Sipakov <sipakov.rv@missioniz.com>
2022-01-13 01:03:32 -0500

    fix: seo for pwa, update favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,9 +5,16 @@ class MyDocument extends Document {
 		return (
 			<Html lang='en'>
 				<Head>
-					<meta charSet='utf-8' />
-					<meta name='mobile-web-app-capable' content='yes' />
-					<meta name='apple-mobile-web-app-capable' content='yes' />
+					<meta charSet='utf-8'
+					/>
+					<meta
+						name='mobile-web-app-capable'
+						content='yes'
+					/>
+					<meta
+						name='apple-mobile-web-app-capable'
+						content='yes'
+					/>
 					<meta
 						name='apple-mobile-web-app-status-bar-style'
 						content='black-translucent'
@@ -22,9 +29,43 @@ class MyDocument extends Document {
 						content='#18181b'
 						media='(prefers-color-scheme: dark)'
 					/>
-					<link rel='apple-touch-icon' href='/static/favicons/maskable_icon_x512.png' />
-					<link rel='icon' type='image/png' href='/static/favicons/maskable_icon_x512.png' />
-					<link rel='manifest' href='/static/favicons/manifest.json' />
+					<meta
+						name="msapplication-TileColor"
+						content="#e4e4e7"
+					/>
+					<meta
+						content="/static/favicons/browserconfig.xml"
+						name="msapplication-config"
+					/>
+					<link
+						href="/static/favicons/favicon-16x16.png"
+						rel="icon"
+						sizes="16x16"
+						type="image/png"
+					/>
+					<link
+						href="/static/favicons/favicon-32x32.png"
+						rel="icon"
+						sizes="32x32"
+						type="image/png"
+					/>
+					<link
+						rel='apple-touch-icon'
+						href='/static/favicons/apple-touch-icon.png'
+					/>
+					<link
+						color="#4a9885"
+						href="/static/favicons/safari-pinned-tab.svg"
+						rel="mask-icon"
+					/>
+					<link
+						href="/static/favicons/favicon.ico"
+						rel="shortcut icon"
+					/>
+					<link
+						href='/static/favicons/manifest.json'
+						rel='manifest'
+					/>
 				</Head>
 				<body>
 				<Main />
