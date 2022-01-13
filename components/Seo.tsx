0785f5d0f8c2806fba4d0f0fe33b720508030b8b
Rostyslav Sipakov <sipakov.rv@missioniz.com>
2022-01-12 21:00:34 -0500

    fix: seo for pwa, add title to _app.tsx

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -57,6 +57,94 @@ export const PageSeo = ({ title, description, availableLocales }) => {
 			<meta name="twitter:description" content={description} />
 			<meta name="twitter:image" content={`${siteMetaData.siteUrl}${siteMetaData.socialBanner}`} />
 			{availableLocales && generateLinks(router, availableLocales)}
+			{/* #region Favicons */}
+			{favicons.map((linkProps) => (
+				<link key={linkProps.href} {...linkProps} />
+			))}
+			{/* #endregion */}
 		</Head>
 	)
 }
+
+type Favicons = {
+	rel: string;
+	href: string;
+	sizes?: string;
+	type?: string;
+};
+
+const favicons: Array<Favicons> = [
+	{
+		rel: 'apple-touch-icon',
+		sizes: '57x57',
+		href: '/static/favicons/apple-icon-57x57.png',
+	},
+	{
+		rel: 'apple-touch-icon',
+		sizes: '60x60',
+		href: '/static/favicons/apple-icon-60x60.png',
+	},
+	{
+		rel: 'apple-touch-icon',
+		sizes: '72x72',
+		href: '/static/favicons/apple-icon-72x72.png',
+	},
+	{
+		rel: 'apple-touch-icon',
+		sizes: '76x76',
+		href: '/static/favicons/apple-icon-76x76.png',
+	},
+	{
+		rel: 'apple-touch-icon',
+		sizes: '114x114',
+		href: '/static/favicons/apple-icon-114x114.png',
+	},
+	{
+		rel: 'apple-touch-icon',
+		sizes: '120x120',
+		href: '/static/favicons/apple-icon-120x120.png',
+	},
+	{
+		rel: 'apple-touch-icon',
+		sizes: '144x144',
+		href: '/static/favicons/apple-icon-144x144.png',
+	},
+	{
+		rel: 'apple-touch-icon',
+		sizes: '152x152',
+		href: '/static/favicons/apple-icon-152x152.png',
+	},
+	{
+		rel: 'apple-touch-icon',
+		sizes: '180x180',
+		href: '/static/favicons/apple-icon-180x180.png',
+	},
+	{
+		rel: 'icon',
+		type: 'image/png',
+		sizes: '192x192',
+		href: '/static/favicons/android-icon-192x192.png',
+	},
+	{
+		rel: 'icon',
+		type: 'image/png',
+		sizes: '32x32',
+		href: '/static/favicons/favicon-32x32.png',
+	},
+	{
+		rel: 'icon',
+		type: 'image/png',
+		sizes: '96x96',
+		href: '/static/favicons/favicon-96x96.png',
+	},
+	{
+		rel: 'icon',
+		type: 'image/png',
+		sizes: '16x16',
+		href: '/static/favicons/favicon-16x16.png',
+	},
+	{
+		rel: 'manifest',
+		href: '/static/favicons/manifest.json',
+	},
+];
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,7 @@ const App = ({ Component, pageProps }: AppProps) => {
 			disableTransitionOnChange
 		>
 			<Head>
+				<title>Rice Bowl</title>
 				<meta
 					name='viewport'
 					content='width=device-width, initial-scale=1, user-scalable=0, viewport-fit=cover'
