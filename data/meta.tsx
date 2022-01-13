74cd10d935f265aa0189fe1e5566758ed6e556bf
Rostyslav Sipakov <sipakov.rv@missioniz.com>
2022-01-13 01:42:37 -0500

    fix: seo for pwa, add favicons as a map in _document

--- data/meta.tsx
+++ data/meta.tsx
@@ -3,8 +3,12 @@ import Head from 'next/head'
 const Meta = () => (
 	<Head>
 		<meta charSet='utf-8' />
-		<meta name='mobile-web-app-capable' content='yes' />
-		<meta name='apple-mobile-web-app-capable' content='yes' />
+		<meta
+			name='mobile-web-app-capable'
+			content='yes' />
+		<meta
+			name='apple-mobile-web-app-capable'
+			content='yes' />
 		<meta
 			name='apple-mobile-web-app-status-bar-style'
 			content='black-translucent'
@@ -22,6 +26,14 @@ const Meta = () => (
 		<meta
 			name='viewport'
 			content='width=device-width, initial-scale=1, user-scalable=0, viewport-fit=cover'
+		/>
+		<meta
+			name="msapplication-TileColor"
+			content="#e4e4e7"
+		/>
+		<meta
+			content="/static/favicons/browserconfig.xml"
+			name="msapplication-config"
 		/>
 		<link
 			href='/static/favicons/manifest.json'
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,39 +5,36 @@ class MyDocument extends Document {
 		return (
 			<Html lang='en'>
 				<Head>
-					<meta
-						name="msapplication-TileColor"
-						content="#e4e4e7"
-					/>
-					<meta
-						content="/static/favicons/browserconfig.xml"
-						name="msapplication-config"
+					<link
+						href='/static/favicons/favicon-16x16.png'
+						rel='icon'
+						sizes='16x16'
+						type='image/png'
 					/>
 					<link
-						href="/static/favicons/favicon-16x16.png"
-						rel="icon"
-						sizes="16x16"
-						type="image/png"
-					/>
-					<link
-						href="/static/favicons/favicon-32x32.png"
-						rel="icon"
-						sizes="32x32"
-						type="image/png"
+						href='/static/favicons/favicon-32x32.png'
+						rel='icon'
+						sizes='32x32'
+						type='image/png'
 					/>
 					<link
 						rel='apple-touch-icon'
 						href='/static/favicons/apple-touch-icon.png'
 					/>
 					<link
-						color="#4a9885"
-						href="/static/favicons/safari-pinned-tab.svg"
-						rel="mask-icon"
+						color='#4a9885'
+						href='/static/favicons/safari-pinned-tab.svg'
+						rel='mask-icon'
 					/>
 					<link
-						href="/static/favicons/favicon.ico"
-						rel="shortcut icon"
+						href='/static/favicons/favicon.ico'
+						rel='shortcut icon'
 					/>
+					{/* #region Favicons */}
+					{favicons.map((linkProps) => (
+						<link key={linkProps.href} {...linkProps} />
+					))}
+					{/* #endregion Favicons */}
 				</Head>
 				<body>
 				<Main />
@@ -47,4 +44,35 @@ class MyDocument extends Document {
 		)
 	}
 }
+
 export default MyDocument
+
+type Favicons = {
+	rel: string;
+	href: string;
+	sizes?: string;
+	type?: string;
+	color?: string;
+};
+
+const favicons: Array<Favicons> = [
+	{
+		href: '/static/favicons/favicon-32x32.png',
+		rel: 'icon',
+		sizes: '32x32',
+		type: 'image/png'
+	},
+	{
+		rel: 'apple-touch-icon',
+		href: '/static/favicons/apple-touch-icon.png'
+	},
+	{
+		color: '#4a9885',
+		href: '/static/favicons/safari-pinned-tab.svg',
+		rel: 'mask-icon'
+	},
+	{
+		href: '/static/favicons/favicon.ico',
+		rel: 'shortcut icon'
+	}
+]
