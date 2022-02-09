3206c18934a6983bde6828b42cdae283a26ef22e
Rostyslav Sipakov <sipakov.rv@missioniz.com>
2022-02-09 18:50:55 -0500

    chore: add FIXME for hover <a>

--- layouts/blog.tsx
+++ layouts/blog.tsx
@@ -48,6 +48,7 @@ export default function BlogLayout({
 								<ViewCounter slug={post.slug} />
 							</p>
 						</div>
+						{/* FIXME: fix hover for <a> */}
 						<div className='w-full max-w-none mt-4 prose dark:prose-dark prose-a:hover:text-[#BAE3FF]'>
 							{children}
 						</div>
